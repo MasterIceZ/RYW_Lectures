f3ef6bfeeee55be8d31fbe5b4de2953363b41a11
AiceAeng <64594596+MasterIceZ@users.noreply.github.com>
2020-07-09 23:33:00 +0700

    Template C

---
+++ M4/Kru_Aof/0-TemplateC.c
@@ -0,0 +1,18 @@
+/*
+  TASK   : TemplateC
+  ARTHOR : IceBorworntat
+  SCHOOL : RYW
+  LANG   :  C
+*/
+//includes
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (){
+  
+  //code
+  
+  return 0;
+}
