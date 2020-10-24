1cef881aa4aae6e930d42b06d765b730fbffc5d5
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-10-24 10:49:57 +0700

    String_1

---
+++ M4/Kru_Aof/String/String_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+char s[1010];
+int main (){
+  scanf(" %s",s);
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;++i){
+    printf("%c",s[i%strlen(s)];
+  }
+  printf("\n");
+  return 0;
+}
