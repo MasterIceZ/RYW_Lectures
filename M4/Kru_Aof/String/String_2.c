e9c826880e72571f6969c0a52f8796fb6ce1a6d5
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-10-24 10:53:03 +0700

    String_2

---
+++ M4/Kru_Aof/String/String_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+char s[1010];
+int main (){
+  scanf(" %s",s);
+  for(int i=0;i<strlen(s);++i){
+    if(s[i]!=s[i+1] && s[i]!=s[i-1]){
+      printf("%c%c",s[i],s[i]);
+    }
+    else{
+      printf("%c",s[i]);
+    }
+  }
+  prinrf("\n");
+  return 0;
+}
