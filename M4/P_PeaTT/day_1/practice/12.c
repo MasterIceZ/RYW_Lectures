d18591076f02551f362656e6c65f8773febc64fb
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-03-22 15:09:43 +0700

    Debug

--- M4/P_PeaTT/day_1/practice/12.c
+++ M4/P_PeaTT/day_1/practice/12.c
@@ -7,11 +7,28 @@
 */
 
 #include<stdio.h>
+#include<ctype.h>
+
 
 int main(){
 	char s;
 	scanf("%c", &s);
-	printf("%c", s+2);
+	if(isupper(s)){
+		if(s!='Y' && s!='Z'){
+			printf("%c",s+2);
+		}
+		else{
+			printf("%c",'A' + abs('X' - s)-1);	
+		}
+	}
+	else{
+		if(s!='y' && s!='z'){
+			printf("%c",s+2);
+		}
+		else{
+			printf("%c",'a' + abs('x' - s)-1);	
+		}
+	}
 	printf("\n");
 	return 0;
 }
